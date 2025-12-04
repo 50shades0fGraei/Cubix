bc53683c64e2387c7f70c0668aeedded486fd0ec
Randall lujan <meta2graei@gmail.com>
2025-12-04 02:58:50 -0800

    H

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { CubeNavigator } from "@/components/cube-navigator"
+
+export default function Home() {
+  return (
+    <main className="w-full h-screen overflow-hidden">
+      <CubeNavigator />
+    </main>
+  )
+}
